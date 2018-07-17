9debdbcd472c6647930009d925a66989c44b0327
Tushar Vengurlekar <tushaarv@gmail.com>
2018-07-17 18:50:40 +0530

    Updated Lesson 3

--- app/lesson3.js
+++ app/lesson3.js
@@ -40,6 +40,27 @@ console.log('end reading Asynchronous file...')
 
 
 // Problem : Callback Hell  Solution: Async.js
-async.parallel(['file1', 'file2', 'file3'], fs.stat, function
-(err, results) {
-})
+// async.parallel(['file1', 'file2', 'file3'], fs.stat, function
+// (err, results) {
+// })
+
+// Promise
+
+// function stats (file) {
+//     return new Promise((resolve, reject) => {
+//       fs.stat(file, (err, data) => {
+//         if (err) {
+//           return reject (err)
+//         }
+//         resolve(data)
+//       })
+//     })
+//   }
+  
+//   Promise.all([
+//     stats('file1'),
+//     stats('file2'),
+//     stats('file3')
+//   ])
+//   .then((data) => console.log(data))
+//   .catch((err) => console.log(err))
